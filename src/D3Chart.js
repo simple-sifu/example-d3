b26f2651dd689972c8095e6dbdd1631025b7eaee
Tommy Han <tommy@Tommys-Air.home>
2020-05-23 20:12:38 -0400

    different heights.

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -14,7 +14,7 @@ export default class D3Chart {
             .attr("x", i * 100)
             .attr("y", 50)
             .attr("width", 50)
-            .attr("height", 400)
+            .attr("height", d)
             .attr("fill", "grey")
         })
 
