e4ec647bb87a177ce58f7dee767c3c5cbfebff9c
Tommy Han <tommy@Tommys-Air.home>
2020-05-23 20:01:05 -0400

    add rectangle.

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -2,6 +2,16 @@ import * as d3 from 'd3'
 
 export default class D3Chart {
     constructor(element){
-        console.log(d3.select(element))
+        const svg = d3.select(element)
+        .append("svg")   //defaults to 300 x 150
+            .attr("width", 500)
+            .attr("height", 500)
+
+        svg.append("rect")
+          .attr("x", 50)
+          .attr("y", 50)
+          .attr("width", 100)
+          .attr("height", 400)
+          .attr("fill", "grey")
     }
 }
