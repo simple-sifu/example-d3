f37ca5551b1f5c521883127910590721e1341d6c
Tommy Han <tommy@Tommys-Air.home>
2020-05-23 20:10:19 -0400

    bars of rectangules.

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,17 +1,22 @@
 import * as d3 from 'd3'
 
+const data = [20, 12, 16, 25, 20];
+
 export default class D3Chart {
     constructor(element){
-        const svg = d3.select(element)
+      const svg = d3.select(element)
         .append("svg")   //defaults to 300 x 150
             .attr("width", 500)
             .attr("height", 500)
 
-        svg.append("rect")
-          .attr("x", 50)
-          .attr("y", 50)
-          .attr("width", 100)
-          .attr("height", 400)
-          .attr("fill", "grey")
+        data.forEach((d,i) => {
+            svg.append("rect")
+            .attr("x", i * 100)
+            .attr("y", 50)
+            .attr("width", 50)
+            .attr("height", 400)
+            .attr("fill", "grey")
+        })
+
     }
 }
